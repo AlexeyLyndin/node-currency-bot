f4285e73335a1012efb63d712c9599dd280cbe85
AlexeyLyndin <lyndin.alexey@gmail.com>
2017-02-02 02:24:18 -0600

    Adding headers to prevent connection error initialized by remote server.

--- controllers/CurrencyController.js
+++ controllers/CurrencyController.js
@@ -6,12 +6,12 @@ class CurrencyController extends TelegramBaseController {
 
     getCurrency($) {
         getUdsCurrency()
-        .then((usd) => {
-            $.sendMessage(usd);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+            .then((usd) => {
+                $.sendMessage(usd);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     }
 
     get routes() {
@@ -22,12 +22,20 @@ class CurrencyController extends TelegramBaseController {
 }
 
 const getUdsCurrency = () => {
+    let request = {
+        url: "http://www.nbrb.by/API/ExRates/Rates/145",
+        json: true,
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+            "Connection": "keep-alive"
+        }
+    };
     return new Promise((resolve, reject) => {
-        req.get({ url: "http://www.nbrb.by/API/ExRates/Rates/145", json: true }, (body, response, err) => {
+        req.get(request, (body, response, err) => {
             if (!err && response.statusCode === 200) {
                 console.log(body);
                 resolve(body);
-            }else{
+            } else {
                 reject(err);
             }
         });
