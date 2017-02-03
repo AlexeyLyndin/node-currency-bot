409a0ee3efc48e5f1529a31533e12219cc1bb3de
AlexeyLyndin <lyndin.alexey@gmail.com>
2017-02-03 03:02:05 -0600

    small refactoring, get default currencies

--- controllers/CurrencyController.js
+++ controllers/CurrencyController.js
@@ -1,18 +1,21 @@
+const _ = require("lodash");
 const req = require("tiny_request");
 const Telegram = require("telegram-node-bot");
 const TelegramBaseController = Telegram.TelegramBaseController;
 
+const urls = ["http://www.nbrb.by/API/ExRates/Rates/145", "http://www.nbrb.by/API/ExRates/Rates/292", "http://www.nbrb.by/API/ExRates/Rates/298"];
+
 class CurrencyController extends TelegramBaseController {
 
     getCurrency($) {
-        getUdsCurrency()
-            .then((usd) => {
-                let result = `${usd.Cur_Name} ${usd.Cur_OfficialRate}`;
-                $.sendMessage(result);
-            })
-            .catch((err) => {
-                console.error(err);
+        getDefaultCurrencies().then((results) => {
+            console.log(results);
+            let response = "";
+            _.each(results, (result) => {
+                response += `${result.Cur_Abbreviation} - ${result.Cur_OfficialRate} \n`;
             });
+            $.sendMessage(response);
+        });
     }
 
     get routes() {
@@ -22,9 +25,20 @@ class CurrencyController extends TelegramBaseController {
     }
 }
 
-const getUdsCurrency = () => {
+const getDefaultCurrencies = () => {
+    let requests = [];
+    _.each(urls, (url) => {
+        let req = performRequest(url);
+        requests.push(req);
+    });
+    return Promise.all(requests).then((responses) => {
+        return responses;
+    });
+};
+
+const performRequest = (url) => {
     let request = {
-        url: "http://www.nbrb.by/API/ExRates/Rates/145",
+        url: url,
         json: true,
         headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
@@ -34,13 +48,13 @@ const getUdsCurrency = () => {
     return new Promise((resolve, reject) => {
         req.get(request, (body, response, err) => {
             if (!err && response.statusCode === 200) {
-                console.log(body);
+                //console.log(body);
                 resolve(body);
             } else {
                 reject(err);
             }
         });
     });
-};
+}
 
 module.exports = CurrencyController;
