3771f7e067324bcd73680afdf1d53f44b3762445
AlexeyLyndin <lyndin.alexey@gmail.com>
2017-02-06 10:10:01 -0600

    parsing partially finished. with a small hack. will be reworked soon.

--- controllers/SelectController.js
+++ controllers/SelectController.js
@@ -10,9 +10,13 @@ const htmlparser = require("htmlparser2");
 
 let rowTag = "";
 let bankNameTag = "";
+let currTag = "";
 
 let bankData = [];
 let bankDataObj;
+let currencyString = "";
+
+let resolve, reject;
 
 const parser = new htmlparser.Parser({
     onopentag: (name, attribs) => {
@@ -31,38 +35,62 @@ const parser = new htmlparser.Parser({
                 bankNameTag = "";
             }
         }
+        if ("span" === name) {
+            if (attribs.class && attribs.class.includes("first_curr")) {
+                currTag = name;
+            }
+        }
 
     },
     ontext: (text) => {
         if (rowTag) {
             if (bankNameTag) {
                 bankDataObj.bankName = text;
-                console.log(`Bank name is ${text}`);
+                //console.log(`Bank name is ${text}`);
+                bankNameTag = "";
                 return;
             }
-            console.log(text);
+            if (currTag) {
+                currencyString += `;${text.trim()}`;
+                currTag = "";
+            }
+
         }
     },
     onclosetag: (tagname) => {
         if (rowTag === tagname) {
             rowTag = "";
-            bankNameTag = ""
-            bankData.push(bankDataObj);
-        }
-        if (bankNameTag) {
             bankNameTag = "";
+            parseValues(currencyString);
+            currencyString = "";
+            bankData.push(bankDataObj);
         }
     },
     onend: () => {
         console.log("end");
-        console.log(bankData);
+        //console.log(bankData);
+        resolve(bankData);
     }
 }, { decodeEntities: true });
+
+const parseValues = (rawString) => {
+    var split = rawString.split(';');
+    //console.log(split);
+    if (split.length !== 8) { return; }
+    bankDataObj.usdBuy = split[2];
+    bankDataObj.usdSell = split[3];
+    bankDataObj.euroBuy = split[4];
+    bankDataObj.euroSell = split[5];
+    bankDataObj.rubBuy = split[6];
+    bankDataObj.rubSell = split[7];
+}
 
 class SelectController extends TelegramBaseController {
 
     select($) {
-        getHtml()
+        getHtml().then((parseData) => {
+            console.log(parseData);
+        });
     }
 
     get routes() {
@@ -73,27 +101,31 @@ class SelectController extends TelegramBaseController {
 }
 
 const getHtml = () => {
-    http.get('http://myfin.by/currency/minsk', (res) => {
-        const statusCode = res.statusCode;
-        const contentType = res.headers['content-type'];
+    return new Promise((res, rej) => {
+        http.get('http://myfin.by/currency/minsk', (response) => {
+            resolve = res;
+            reject = rej;
+            const statusCode = response.statusCode;
+            const contentType = response.headers['content-type'];
 
-        let error;
-        if (statusCode !== 200) {
-            error = new Error(`Request Failed.\n` +
-                `Status Code: ${statusCode}`);
-        }
-        if (error) {
-            console.log(error.message);
-            // consume response data to free up memory
-            res.resume();
-            return;
-        }
+            let error;
+            if (statusCode !== 200) {
+                error = new Error(`Request Failed.\n` +
+                    `Status Code: ${statusCode}`);
+            }
+            if (error) {
+                console.log(error.message);
+                // consume response data to free up memory
+                response.resume();
+                return;
+            }
 
 
-        res.setEncoding('utf8');
-        res.pipe(parser);
-    }).on('error', (e) => {
-        console.log(`Got error: ${e.message}`);
+            response.setEncoding('utf8');
+            response.pipe(parser);
+        }).on('error', (e) => {
+            console.log(`Got error: ${e.message}`);
+        });
     });
 }
 
